b2d38b099edb49f5201788aa8027c49d694b304a
DatPika <levitind@gmail.com>
2024-09-04 16:25:43 -0400

    Lab2

---
+++ Assets/Scripts/TrapController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class TrapController : MonoBehaviour
+{
+    public float rotationSpeed = 100f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Quaternion deltaRotation = Quaternion.Euler(0, rotationSpeed * Time.fixedDeltaTime, 0);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+    }
+}
